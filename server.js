cd51573977c29a459e717001272f1c88d956387b
relieyanhilman <relieyanhilman93@gmail.com>
2021-06-15 11:35:55 +0800

    Initial Author Routes

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ const expressLayouts = require('express-ejs-layouts');
 
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(express.json())
+app.use(express.urlencoded({ limit: '10mb', extended:false }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true})
@@ -22,5 +25,6 @@ db.on('error', error=> console.error(error))
 db.once('open', () => console.log('Connected to mongooges'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000);
