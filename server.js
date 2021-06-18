c88ab3b9e23f77270dec4c525f45d185a9c07298
relieyanhilman <relieyanhilman93@gmail.com>
2021-06-18 16:54:28 +0800

    menambahkan index dan buku baru

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const expressLayouts = require('express-ejs-layouts');
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -21,10 +22,11 @@ app.use(express.urlencoded({ limit: '10mb', extended:false }))
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection
-db.on('error', error=> console.error(error))
+db.on('error', error=> console.error(error)) 
 db.once('open', () => console.log('Connected to mongooges'))
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 app.listen(process.env.PORT || 3000);
