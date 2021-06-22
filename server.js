49ac0e5e997f615a059bca32970630698759f4f1
relieyanhilman <relieyanhilman93@gmail.com>
2021-06-22 16:43:04 +0800

    udah nambahin fitur CRUD ke author

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ if(process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const methodOverride = require('method-override')
 
 
 const indexRouter = require('./routes/index')
@@ -15,6 +16,7 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
+app.use(methodOverride('_method'))
 app.use(express.static('public'));
 app.use(express.json())
 app.use(express.urlencoded({ limit: '10mb', extended:false }))
